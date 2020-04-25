3ab765949cebf1ee804bc6c18f27cfc664c8861c
Jack <git@jackhiggins.ie>
2020-04-25 19:04:02 +0200

    get config from logger and more comments

--- water-filter-sensor.ino
+++ water-filter-sensor.ino
@@ -1,11 +1,9 @@
 /*
  * CALIBRATION VALUES
- * 
- * All of these must be reset after disassembling and reassembling the base.
  */
 
 /*
- * Turn on debug output to allow for calibration.
+ * Turn on debug output to allow for console calibration.
  * 
  * Values format:
  * MINIMUM_WATER_LEVEL, MAXIMUM_WATER_LEVEL, CURRENT_WEIGHT, CURRENT_WEIGHT_SMOOTHED_AVERAGE
@@ -13,7 +11,8 @@
 const boolean DEBUG_ON = true;
 
 /*
- * The maximum weight of the system. Above this level, water will overflow. Updated by the data logger.
+ * The maximum weight of the system. Above this level, water will overflow.
+ * This is a default value which is updated at first connection to the data logger.
  * 
  * (Residual in top tank + full lower tank)
  */
@@ -21,6 +20,7 @@ int MAXIMUM_WATER_LEVEL = 121000;
 
 /*
  * The weight of the system when tap no longer flows out. Updated by the data logger.
+ * This is a default value which is updated at first connection to the data logger.
  * 
  * (Residual in top tank + residual in lower tank)
  */
@@ -87,7 +87,7 @@ const int MOVING_AVERAGE_BUCKET_SIZE = 20;
 const int LOAD_SENSOR_TRANSMIT_INTERVAL = 50;
 
 /*
- * Screen dimensions
+ * Screen dimensions and setup
  */
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -115,6 +115,7 @@ const int LOAD_SENSOR_TRANSMIT_INTERVAL = 50;
 
 #include <movingAvg.h>
 #include "HX711.h"
+#include <ArduinoJson.h>
 #include <SPI.h>
 #include <Wire.h>
 #include<ESP8266WiFi.h> 
@@ -126,13 +127,14 @@ HX711 load_sensor;
 WiFiClient client;
 IPAddress server(IP_1,IP_2,IP_3,IP_4);
 movingAvg smooth_weight(MOVING_AVERAGE_BUCKET_SIZE);
+StaticJsonDocument<200> doc;
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-int transmit_cycle = 0;
+int transmit_cycle = LOAD_SENSOR_TRANSMIT_INTERVAL;
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   setupScreen();
   drawText("Booting...", 3);
@@ -152,11 +154,10 @@ void setup() {
 
 void setupScreen() {
   
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
-  
 }
 
 int readWeight() {
@@ -177,7 +178,6 @@ int readWeight() {
   }
     
   return smooth_weight.getAvg();
-
 }
 
 void drawText(String text, int textSize) {
@@ -189,12 +189,12 @@ void drawText(String text, int textSize) {
   display.println(text);
 
   display.display();
-  delay(50);
+  delay(50); // Delay added because writing to the screen too frequently causes it not to update at all
 }
 
 int getCurrentFillPercent(int current_weight) {
   if(current_weight <= MINIMUM_WATER_LEVEL){
-    return 0;
+    return 0; // Negative fill percentages don't make sense - usually this will occur only when the top tank is taken off
   }
 
   double fraction = ((double)current_weight - (double)MINIMUM_WATER_LEVEL) / ((double)MAXIMUM_WATER_LEVEL - (double)MINIMUM_WATER_LEVEL) * 100;
@@ -213,7 +213,6 @@ void loop() {
   drawText(percentText, 5);
 
   transmitReading(currentWeight, fillPercent);
-
 }
 
 void transmitReading(int weight, int fillPercent) {
@@ -223,10 +222,36 @@ void transmitReading(int weight, int fillPercent) {
       transmit_cycle = 0;
     }
 
-    String json = "{\"load\":"+String(weight)+",\"fillPercentage\":"+String(fillPercent)+"}";
-    
+    // Get the latest configuration for high/low levels and update if necessary
+    if (client.connect(server, IP_PORT)) {      
+      client.print("GET /kitchen/water-filter/configuration HTTP/1.1\r\nHost: hallway\r\nConnection: close\r\n\r\n");
+
+      String httpConversation = "";      
+      while (client.connected() || client.available())
+      {
+        if (client.available())
+        {
+          httpConversation += client.readStringUntil('\n');
+        }
+      }
+      client.stop();
+
+      String allJson = httpConversation.substring(httpConversation.indexOf('{'), httpConversation.indexOf('}') + 1);
+      Serial.println(allJson);
+
+      deserializeJson(doc, allJson);
+      MAXIMUM_WATER_LEVEL = doc["fullLevel"];
+      MINIMUM_WATER_LEVEL = doc["emptyLevel"];
+      
+    }else{
+      Serial.println("ERROR: Failed to connect");
+      reboot();
+    }
+
+    // Try to send the latest reading to the logger
     if (client.connect(server, IP_PORT)) {      
       // Make a HTTP request:
+      String json = "{\"load\":"+String(weight)+",\"fillPercentage\":"+String(fillPercent)+"}";
       client.println("POST /kitchen/water-filter HTTP/1.0");
       client.println("Content-Type: application/json");
       client.print("Content-Length: ");
@@ -251,9 +276,8 @@ void reboot(){
   ESP.restart();
 }
 
-
 void connectToWifi(){
-  String wifiConnectionInfo = "Connectingto WiFi"; //Newline wrap on the OLED makes this appear correctly
+  String wifiConnectionInfo = "Connectingto WiFi"; //The newline wrap on the 128x64 OLED makes this appear correctly
   
   if(WiFi.status() == WL_CONNECTED){
     return;  
@@ -270,7 +294,8 @@ void connectToWifi(){
   int rebootCountdown = WIFI_CONNECT_TIMEOUT_SECONDS * 1000;
 
   String dotsString = "";
-  
+
+  // Displays a nice progression on the screen until the connection can be established
   while (WiFi.status() != WL_CONNECTED) {
 
     if(dotsString.length() < 3){
